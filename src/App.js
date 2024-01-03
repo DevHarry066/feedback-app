cf50f659affda51549fee7468ff70eeaf1bbc982
Hariom <82692072+DevHarry066@users.noreply.github.com>
2024-01-03 10:21:33 -0800

    Basic Router and Navigation in React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
@@ -6,6 +7,8 @@ import FeedbackData from './Data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './Pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -20,14 +23,23 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header text="React UI" />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          }>
+          </Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+      <AboutIconLink />
+    </Router>
   )
 }
 
